8e4b57d8d767a3f7838edeb04703e381167b509c
James Oakes <cheerlesssnake@gmail.com>
2024-07-29 22:52:32 -0500

    added CoverPoints

--- Assets/Scripts/BasicNavAgent.cs
+++ Assets/Scripts/BasicNavAgent.cs
@@ -6,16 +6,19 @@ using UnityEngine.AI;
 public class BasicNavAgent : MonoBehaviour
 {
     NavMeshAgent agent;
+    GameObject[] CoverPoints;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        CoverPoints = GameObject.FindGameObjectsWithTag("CoverPoint");
+        agent.destination = CoverPoints[Random.Range(0, CoverPoints.Length)].transform.position;
         //agent.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
+        //agent.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
     }
 }
